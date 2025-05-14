0faae2fb72065abd8ad842f63f789cf9a11d5ec6
1mmhg <eaglechen2020@gmail.com>
2025-05-14 19:33:24 +0800

    modify Blogs.vue

--- src/views/Blogs.vue
+++ src/views/Blogs.vue
@@ -3,21 +3,48 @@ import { ref, onMounted } from 'vue'
 import axios from 'axios'
 
 const blogs = ref([])
+const newBlogName = ref("")
+const selectedBlog = ref(null)
 
-onMounted(async () => {
-    const res = await axios.get('http://localhost:8000/blogs')
+const fetchBlogs = async () => {
+    const res = await axios.get('http://localhost:8000/blogs/')
     blogs.value = res.data
-})
+}
+
+const addBlog = async () => {
+    const res = await axios.post('http://localhost:8000/blogs/', {
+        name: newBlogName.value
+    })
+    blogs.value.push(res.data)
+    newBlogName.value = ""
+}
+
+const fetchOneBlog = async (id) => {
+    const res = await axios.get(`http://localhost:8000/blogs/${id}`)
+    selectedBlog.value = res.data
+}
+
+onMounted(fetchBlogs)
 </script>
 
 <template>
     <div>
-        <h2>Blog 列表</h2>
+        <h2>所有 Blog</h2>
         <ul>
-            <li v-for="(b, i) in blogs" :key="i">
-                <h3>{{ b.title }}</h3>
-                <p>{{ b.content }}</p>
+            <li v-for="b in blogs" :key="b.id">
+                {{ b.name }}
+                <button @click="fetchOneBlog(b.id)">詳細</button>
             </li>
         </ul>
+
+        <div v-if="selectedBlog">
+            <h3>Blog 詳細</h3>
+            <p>ID: {{ selectedBlog.id }}</p>
+            <p>Name: {{ selectedBlog.name }}</p>
+        </div>
+
+        <h3>新增 Blog</h3>
+        <input v-model="newBlogName" placeholder="輸入文章名稱" />
+        <button @click="addBlog">新增</button>
     </div>
 </template>
